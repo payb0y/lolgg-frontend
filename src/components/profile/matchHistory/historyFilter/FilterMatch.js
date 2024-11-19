001ee1d5440ff95259297e0a065148b4d1967420
payb0y <othmane.gat@gmail.com>
2024-11-19 03:09:31 +0100

    fix normal game ID+

--- src/components/profile/matchHistory/historyFilter/FilterMatch.js
+++ src/components/profile/matchHistory/historyFilter/FilterMatch.js
@@ -1,27 +1,26 @@
 import { Select, MenuItem } from "@mui/material";
 const FilterMatch = ({ matchType, queueChangeHandler }) => {
-    const MATCH_TYPES = {
-        0: "All",
-        420: "Ranked Solo",
-        440: "Ranked Flex",
-        400: "Normal Draft",
-        430: "Normal Blind",
-        450: "ARAM",
-        700: "Clash",
-    };
-    return (
-        <Select
-            autoWidth
-            value={matchType}
-            defaultValue={"EUW"}
-            onChange={queueChangeHandler}
-        >
-            {Object.keys(MATCH_TYPES).map((type) => (
-                <MenuItem key={type} value={type}>
-                    {MATCH_TYPES[type]}
-                </MenuItem>
-            ))}
-        </Select>
-    );
+  const MATCH_TYPES = {
+    0: "All",
+    420: "Ranked Solo",
+    440: "Ranked Flex",
+    400: "Normal",
+    450: "ARAM",
+    700: "Clash",
+  };
+  return (
+    <Select
+      autoWidth
+      value={matchType}
+      defaultValue={"EUW"}
+      onChange={queueChangeHandler}
+    >
+      {Object.keys(MATCH_TYPES).map((type) => (
+        <MenuItem key={type} value={type}>
+          {MATCH_TYPES[type]}
+        </MenuItem>
+      ))}
+    </Select>
+  );
 };
 export default FilterMatch;
